199e3c5842e389ef790990bb3e6aea9fc4e445fb
Kurt Landrus <klandrus@gmail.com>
2015-08-07 09:31:58 -0400

    add react state helpers

--- config/application.rb
+++ config/application.rb
@@ -31,5 +31,8 @@ module Accounts
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    # enable react state helpers
+    config.react.addons = true
   end
 end
